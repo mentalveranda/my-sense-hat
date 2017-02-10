07e6fd61c87f3aaca9535759ce7f4535ec4cd097
MichaelNYC <mtalbot@Nomad-XII.local>
2017-02-10 12:50:13 -0500

    added rotation example

--- space_invader.py
+++ space_invader.py
@@ -4,3 +4,18 @@ from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
 sense.load_image("space_invader.png")
+
+sleep(1)
+
+sense.set_rotation(180)
+# alternatives
+sense.rotation = 180
+sense.clear()
+
+sleep(1)
+
+sense.load_image("space_invader.png")
+sleep(1)
+
+
+sense.clear()

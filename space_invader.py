5f4606ee77e1bcc2ffcc231338ed0c1b8092891c
MichaelNYC <mtalbot@Nomad-XII.local>
2017-02-10 12:56:50 -0500

    added rotation

--- space_invader.py
+++ space_invader.py
@@ -4,19 +4,34 @@ from time import sleep
 
 sense = SenseHat()
 sense.clear()
-sense.load_image("space_invader.png")
 
+sense.set_rotation(90)
+
+sense.load_image("space_invader.png")
 sleep(1)
 
 sense.set_rotation(180)
+
+#sense.load_image("space_invader.png")
+sleep(1)
+
+sense.set_rotation(270)
+
+#sense.load_image("space_invader.png")
+sleep(1)
+sense.set_rotation(0)
+
+#sense.load_image("space_invader.png")
+sleep(1)
+
+#sense.set_rotation(180)
 # alternatives
-sense.rotation = 180
-sense.clear()
+#sense.rotation = 180
+#sense.clear()
 
-sleep(1)
+#sleep(1)
 
-sense.load_image("space_invader.png")
-sleep(1)
+#sleep(1)
 
 
 sense.clear()
